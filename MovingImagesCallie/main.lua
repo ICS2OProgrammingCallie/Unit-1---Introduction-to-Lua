2af987d741d7be3d7415bfdf3fb0b04ed5622636
calliemcwaters <callie.mcwaters@ocsbstudent.ca>
2018-10-02 14:44:41 -0400

    added code

--- MovingImagesCallie/main.lua
+++ MovingImagesCallie/main.lua
@@ -20,7 +20,7 @@ beetleship.alpha = 0
 
 -- set the initial x and y position of beetleship
 beetleship.x = 0
-beetleship.y = display.contentHeight/3
+beetleship.y = display.contentHeight*1/3
 
 -- Function: MoveShip
 -- Input: this function accepts an event listener
@@ -49,16 +49,17 @@ star.alpha = 0
 
 -- set the initial x and y position of beetleship
 star.x = 0
-star.y = display.contentHeight/3
-
+star.y = display.contentHeight*3/3
 -- Function: MoveShip
 -- Input: this function accepts an event listener
 -- Output: none
 -- Description: this function adds the scroll speed to the x-value of the ship
 local function MoveStar(event)
 	-- adds the scroll speed to the x-value of the star
-	star.x = star.x + scrollSpeed
+	star.x = star.x - scrollSpeed
 	-- change the transperency of the ship every time it ,moves so that it fades out
 	star.alpha = star.alpha + 0.01
 end
 
+-- MoveShip will be called over and over again
+Runtime:addEventListener("enterFrame", MoveStar)
