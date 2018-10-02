513059cea9817dc90ffe6cb81ad14961f2c7a72a
calliemcwaters <callie.mcwaters@ocsbstudent.ca>
2018-10-02 14:29:49 -0400

    added star

--- MovingImagesCallie/main.lua
+++ MovingImagesCallie/main.lua
@@ -13,7 +13,7 @@ scrollSpeed = 3
 local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
 
 -- character image with width and height
-local beetleship = display.newImageRect ("Images/beetleship.png", 200, 200)
+local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
 
 -- set the image to be transpearent
 beetleship.alpha = 0
@@ -35,3 +35,30 @@ end
 
 -- MoveShip will be called over and over again
 Runtime:addEventListener("enterFrame", MoveShip)
+
+-------------------------------------------------------------------------------------------------------------
+
+-- global variables
+scrollSpeed = 3
+
+-- character image with width and height
+local star = display.newImageRect("Images/star.png", 500, 500)
+
+-- set the image to be transperent
+star.alpha = 0
+
+-- set the initial x and y position of beetleship
+star.x = 0
+star.y = display.contentHeight/3
+
+-- Function: MoveShip
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: this function adds the scroll speed to the x-value of the ship
+local function MoveStar(event)
+	-- adds the scroll speed to the x-value of the star
+	star.x = star.x + scrollSpeed
+	-- change the transperency of the ship every time it ,moves so that it fades out
+	star.alpha = star.alpha + 0.01
+end
+
