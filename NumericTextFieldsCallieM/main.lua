e4bfbc23c46f89ad83f5db1b0efe6bb033becc95
calliemcwaters <callie.mcwaters@ocsbstudent.ca>
2018-10-16 14:30:47 -0400

    added code

--- NumericTextFieldsCallieM/main.lua
+++ NumericTextFieldsCallieM/main.lua
@@ -1,7 +1,72 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: Numeric Textfield
+-- Name: Callie McWaters
+-- Course: ICS20
+-- This program displays a math question and asks the user to answer in a numeric textfield
+----------------------------------------------------------------------------------------
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- sets the background colour
+display.setDefault("background", 204/255, 0/255, 102/255)
+
+----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+----------------------------------------------------------------------------------------
+
+-- create local variables
+local questionObject
+local correctObject
+local numericField
+local randomNumber1
+local randomNumber2
+local userAnswer
+local correctAnswer
+
+----------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+----------------------------------------------------------------------------------------
+
+local function AskQuestion()
+	-- generate 2 random numbers between a max. and a min. number
+	randomNumber1 = math.random(0, 10)
+	randomNumber2 = math.random(0, 10)
+
+	correctAnswer = randomNumber1 + randomNumber2
+
+	-- create question in text object
+	questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+
+end
+
+local function HideCorrect()
+	correctObject.isVisible = false
+	AskQuestion()
+end 
+
+local function numericFieldListener( event )
+
+	-- User begins editing "numericField"
+	if ( event.phase == "began" ) then
+
+		--clear text field
+		event.target.text = ""
+
+	elseif evwent.phase == "submitted" then
+
+		-- when the answer is submitted (enter key is pressed) set user input to user's answer
+		userAnswer = tonumber(event.target.text)
+
+		-- if the users answer and the correct answer are the same:
+		if (userAnswer == correctAnswer) then
+			correctObject.isVisible = true
+			timer.performWithDelay(2000, HideCorrect)
+
+		end
+	end
+end
+
+----------------------------------------------------------------------------------------
+-- OBJECT CREATION
+----------------------------------------------------------------------------------------
