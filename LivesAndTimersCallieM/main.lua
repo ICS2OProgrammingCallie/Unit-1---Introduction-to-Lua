f6ad63d11ef22c8997c6f7c539af81e5fd29ec78
calliemcwaters <callie.mcwaters@ocsbstudent.ca>
2018-10-22 14:27:22 -0400

    added code

--- LivesAndTimersCallieM/main.lua
+++ LivesAndTimersCallieM/main.lua
@@ -9,7 +9,7 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 -- sets the background colour
-display.setDefault("background", 204/255, 0/255, 102/255)
+display.setDefault("background", 163/255, 222/255, 243/255)
 
 ----------------------------------------------------------------------------------------
 -- LOCAL VARIABLES
@@ -28,6 +28,16 @@ local incorrectAnswer
 local numberOfPoints = 0
 local points
 
+-- variables for the timer
+local totalSeconds = 5
+local secondsLeft = 5
+local clockText
+local countDownTimer
+local lives = 4
+local heart1
+local heart2
+local heart3
+local heart4
 ----------------------------------------------------------------------------------------
 -- SOUNDS
 ----------------------------------------------------------------------------------------
@@ -111,6 +121,40 @@ local function NumericFieldListener( event )
 	end
 end
 
+local function UpdateTime()
+
+	-- decrement the number of seconds
+	secondsLeft = secondsLeft - 1
+
+	-- display the number of seconds left in the clock object
+	clockText.text = secondsLeft .. ""
+
+	if (secondsLeft == 0 ) then
+		-- reset the number of seconds left
+		secondsLeft = totalSeconds
+		lives = lives - 1
+	end
+end
+
+local function DecreaseLives()
+	
+	-- take away a heart everytime you lose a life
+	if (lives == 4) then
+		heart4.isVisible = false
+	elseif (lives == 3) then
+		heart3.isVisible = false
+	elseif (lives == 2) then
+		heart2.isVisible = false
+	elseif (lives == 1) then
+		heart1.isVisible = false
+	end
+end
+
+-- function that calls the timer
+local function startTimer()
+	-- create a countdown timer that loops infinitely
+	countDownTimer = timer.performWithDelay( 1000, UpdateTime, 0)
+end
 ----------------------------------------------------------------------------------------
 -- OBJECT CREATION
 ----------------------------------------------------------------------------------------
@@ -131,7 +175,7 @@ incorrectObject.isVisible = false
 
 -- Create numeric field
 NumericField = native.newTextField( display.contentWidth/2, display.contentHeight/1.5, 200, 80)
-NumericField.inputType = "number"
+NumericField.inputType = "display"
 
 -- add the event listener for the numeric field
 NumericField:addEventListener( "userInput", NumericFieldListener )
@@ -140,9 +184,30 @@ NumericField:addEventListener( "userInput", NumericFieldListener )
 points = display.newText( "Correct = ".. numberOfPoints, display.contentWidth/3.5, display.contentHeight/5, nil, 50)
 points:setTextColor(30/255, 219/255, 188/255)
 
+-- create the lives to display on the screen
+heart1 = display.newImageRect("Images/heart.png", 100, 100)
+heart1.x = display.contentWidth * 7 / 8
+heart1.y = display.contentHeight * 1 / 7
+
+heart2 = display.newImageRect("Images/heart.png", 100, 100)
+heart2.x = display.contentWidth * 6 / 8
+heart2.y = display.contentHeight * 1 / 7
+
+heart3 = display.newImageRect("Images/heart.png", 100, 100)
+heart3.x = display.contentWidth * 5 / 8
+heart3.y = display.contentHeight * 1 / 7
+
+heart4 = display.newImageRect("Images/heart.png", 100, 100)
+heart4.x = display.contentWidth * 4 / 8
+heart4.y = display.contentHeight * 1 / 7
+
+
 ----------------------------------------------------------------------------------------
 -- FUNCTION CALLS
 ----------------------------------------------------------------------------------------
 
 -- call the function to ask the question
 AskQuestion()
+
+-- call the function to decrease lives
+DecreaseLives()
