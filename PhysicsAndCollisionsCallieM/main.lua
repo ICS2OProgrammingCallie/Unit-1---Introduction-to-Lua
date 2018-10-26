db53e6602051cb62dcd5e9dd7fbe0310462146a4
calliemcwaters <callie.mcwaters@ocsbstudent.ca>
2018-10-26 14:26:45 -0400

    added code

--- PhysicsAndCollisionsCallieM/main.lua
+++ PhysicsAndCollisionsCallieM/main.lua
@@ -2,7 +2,25 @@
 -- Title: Physics and Collisions
 -- Name: Callie McWaters
 -- Course: ICS20
--- Thsi 
+-- This program displays a ball that bounces down a beam using physics
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- load physics
+local physics = require("physics")
+
+-- start physics
+physics.start()
+
+-----------------------------------------------------------------------------------------
+-- objects
+-----------------------------------------------------------------------------------------
+
+-- ground
+local ground = display.newImage("Images/ground.png", 0, 0)
+ground.x = display.contentWidth/2
+ground.y = display.contentHeight
+-- set width to be same as the screen
+ground.width = display.contentWidth
